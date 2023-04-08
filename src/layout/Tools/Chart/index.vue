c12ab1dd1e470c275a1e89460016c1c8f00dcec7
moneyinto <woyaoqianjin@gmail.com>
2023-04-08 12:55:08 +0800

    图表数据编辑表格UI

--- src/layout/Tools/Chart/index.vue
+++ src/layout/Tools/Chart/index.vue
@@ -6,50 +6,142 @@
         okText="确认"
         cancelText="取消"
         class="ppt-chart-dialog"
+        :visible="visible"
+        @cancel="close"
         @ok="sure"
     >
-        <div class="chart-editor-content">
-            <table>
-                <tbody>
-                    <tr v-for="rowIndex in 31" :key="rowIndex">
-                        <td
-                            v-for="colIndex in 30"
-                            :key="colIndex"
+        <div class="chart-hidden-box">
+            <div class="chart-editor-content" tabindex="1" onkeydown.stop="">
+                <table>
+                    <tbody>
+                        <tr
+                            v-for="rowIndex in 31"
+                            :key="rowIndex"
+                            :class="{
+                                'table-fixed-top': rowIndex === 1
+                            }"
                         >
-                            <input
-                                :class="[
-                                    'item'
-                                ]"
-                                :disabled="rowIndex === 1 && colIndex === 1"
-                                :id="`cell-${rowIndex - 1}-${colIndex - 1}`"
-                                autocomplete="off"
-                                @focus="
-                                    focusCell = [rowIndex - 1, colIndex - 1]
-                                "
-                            />
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+                            <td
+                                v-for="colIndex in 27"
+                                :key="colIndex"
+                                :class="{
+                                    'chart-table-blue': rowIndex === 2 || colIndex === 2,
+                                    'no-top-border': rowIndex < 3,
+                                    'no-left-border': colIndex < 3,
+                                    'no-right-border': colIndex === 1 || colIndex === 27,
+                                    'no-bottom-border': rowIndex === 1 || rowIndex === 31,
+                                    'table-fixed-left': colIndex === 1
+                                }"
+                            >
+                                <input
+                                    :class="['item']"
+                                    v-if="rowIndex > 1 && colIndex > 1"
+                                    :disabled="rowIndex === 2 && colIndex === 2"
+                                    :id="`cell-${rowIndex - 1}-${colIndex - 1}`"
+                                    autocomplete="off"
+                                    @focus="
+                                        focusCell = [rowIndex - 2, colIndex - 2]
+                                    "
+                                />
+                                <div
+                                    class="chart-table-td"
+                                    :class="{
+                                        'border-bottom': rowIndex === 1,
+                                        'border-right': colIndex === 1
+                                    }"
+                                    v-else
+                                >
+                                    <span v-if="colIndex === 1 && rowIndex > 1">
+                                        {{ rowIndex - 1 }}
+                                    </span>
+                                    <span
+                                        v-else-if="
+                                            colIndex > 1 && rowIndex === 1
+                                        "
+                                    >
+                                        {{ String.fromCharCode(colIndex + 63) }}
+                                    </span>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </a-modal>
 </template>
 
 <script lang="ts" setup>
-import { IPPTChartElement } from "@/types/element";
-import { PropType, ref, toRefs } from "vue";
+import { ChartData, IPPTChartElement } from "@/types/element";
+import { PropType, nextTick, ref, toRefs, watch } from "vue";
 
-const emit = defineEmits(["ok"]);
+const emit = defineEmits(["ok", "update:visible"]);
+
+const baseChartData: ChartData = {
+    labels: ["类别1", "类别2", "类别3", "类别4", "类别5"],
+    legends: ["系列1"],
+    series: [[12, 19, 5, 2, 18]]
+};
 
 const props = defineProps({
     element: {
         type: Object as PropType<IPPTChartElement>
+    },
+    visible: {
+        type: Boolean,
+        required: true
     }
 });
 
-const { element } = toRefs(props);
+const { visible } = toRefs(props);
 
 const focusCell = ref<[number, number] | null>(null);
+
+const initData = () => {
+    const _data: string[][] = [];
+
+    const { labels, legends, series } = props.element?.data || baseChartData;
+    console.log(legends);
+    const rowCount = labels.length;
+    const colCount = series.length;
+
+    _data.push(["", ...legends]);
+    for (let rowIndex = 0; rowIndex < rowCount; rowIndex++) {
+        const row = [labels[rowIndex]];
+        for (let colIndex = 0; colIndex < colCount; colIndex++) {
+            row.push(series[colIndex][rowIndex] + "");
+        }
+        _data.push(row);
+    }
+
+    console.log(_data);
+
+    for (let rowIndex = 0; rowIndex < rowCount + 1; rowIndex++) {
+        for (let colIndex = 0; colIndex < colCount + 1; colIndex++) {
+            const inputRef = document.querySelector(
+                `#cell-${rowIndex + 1}-${colIndex + 1}`
+            ) as HTMLInputElement;
+            console.log(inputRef);
+            if (!inputRef) continue;
+            inputRef.value = _data[rowIndex][colIndex] + "";
+        }
+    }
+};
+
+watch(
+    () => props.visible,
+    () => {
+        if (props.visible) {
+            nextTick(initData);
+        } else {
+            // clear
+        }
+    }
+);
+
+const close = () => {
+    emit("update:visible", false);
+};
 
 const sure = () => {
     emit("ok");
@@ -72,6 +164,13 @@ const sure = () => {
     }
 }
 
+.chart-hidden-box {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    outline: 1px solid #ccc;
+}
+
 .chart-editor-content {
     width: 100%;
     height: 100%;
@@ -84,12 +183,15 @@ const sure = () => {
         table-layout: fixed;
         border-collapse: collapse;
         border-spacing: 0;
+        // margin: -1px;
         td {
             text-align: center;
             border: 1px solid #ccc;
             vertical-align: middle;
             width: 100px;
             height: 32px;
+            overflow: hidden;
+            padding: 0;
         }
         .item {
             width: 100%;
@@ -100,6 +202,50 @@ const sure = () => {
             text-align: center;
             background-color: transparent;
         }
+        td.no-right-border {
+            border-right: 0;
+        }
+        td.no-bottom-border {
+            border-bottom: 0;
+        }
+        td.no-top-border {
+            border-top: 0;
+        }
+        td.no-left-border {
+            border-left: 0;
+        }
     }
+}
+
+.chart-table-td {
+    background-color: #f7f7f7;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #666;
+    font-size: 13px;
+    // margin: -2px -1px 0;
+    &.border-bottom {
+        border-bottom: 1px solid #ccc;
+    }
+    &.border-right {
+        border-right: 1px solid #ccc;
+    }
+}
+
+.chart-table-blue {
+    background-color: rgba(200, 200, 200, 0.3);
+}
+
+.table-fixed-top {
+    position: sticky;
+    top: 0;
+    z-index: 1;
+}
+
+.table-fixed-left {
+    position: sticky;
+    left: 0;
 }
 </style>
