6b2bb5ff6bbe3f506a3f26e0207c964844c7a4cb
moneyinto <woyaoqianjin@gmail.com>
2023-04-25 18:58:00 +0800

    导出文本形状增加阴影

--- src/utils/export.ts
+++ src/utils/export.ts
@@ -1,5 +1,6 @@
 import {
     IPPTElementOutline,
+    IPPTElementShadow,
     IPPTShapeElement,
     IPPTTextElement
 } from "@/types/element";
@@ -64,44 +65,93 @@ type Points = Array<
     | { close: true }
 >;
 
+// 获取阴影配置
+const getShadowOption = (shadow: IPPTElementShadow): Pptxgen.ShadowProps => {
+    const { h, v } = shadow;
+
+    let offset = 4;
+    let angle = 45;
+
+    if (h === 0 && v === 0) {
+        offset = 4;
+        angle = 45;
+    } else if (h === 0) {
+        if (v > 0) {
+            offset = v;
+            angle = 90;
+        } else {
+            offset = -v;
+            angle = 270;
+        }
+    } else if (v === 0) {
+        if (h > 0) {
+            offset = h;
+            angle = 1;
+        } else {
+            offset = -h;
+            angle = 180;
+        }
+    } else if (h > 0 && v > 0) {
+        offset = Math.max(h, v);
+        angle = 45;
+    } else if (h > 0 && v < 0) {
+        offset = Math.max(h, -v);
+        angle = 315;
+    } else if (h < 0 && v > 0) {
+        offset = Math.max(-h, v);
+        angle = 135;
+    } else if (h < 0 && v < 0) {
+        offset = Math.max(-h, -v);
+        angle = 225;
+    }
+
+    return {
+        type: "outer",
+        color: shadow.color.replace("#", ""),
+        blur: shadow.blur * PT_PX_RATIO,
+        offset,
+        angle
+    };
+};
+
 const formatPoints = (points: SvgPoints): Points => {
     return points.map((point) => {
         if (point.close !== undefined) {
             return { close: true };
         } else if (point.type === "M") {
             return {
-                x: ((point.x || 0) / INCH_PX_RATIO),
-                y: ((point.y || 0) / INCH_PX_RATIO),
+                x: (point.x || 0) / INCH_PX_RATIO,
+                y: (point.y || 0) / INCH_PX_RATIO,
                 moveTo: true
             };
         } else if (point.curve) {
             if (point.curve.type === "cubic") {
                 return {
-                    x: ((point.x || 0) / INCH_PX_RATIO),
-                    y: ((point.y || 0) / INCH_PX_RATIO),
+                    x: (point.x || 0) / INCH_PX_RATIO,
+                    y: (point.y || 0) / INCH_PX_RATIO,
                     curve: {
                         type: "cubic",
-                        x1: ((point.curve.x1 as number) / INCH_PX_RATIO),
-                        y1: ((point.curve.y1 as number) / INCH_PX_RATIO),
-                        x2: ((point.curve.x2 as number) / INCH_PX_RATIO),
-                        y2: ((point.curve.y2 as number) / INCH_PX_RATIO)
+                        x1: (point.curve.x1 as number) / INCH_PX_RATIO,
+                        y1: (point.curve.y1 as number) / INCH_PX_RATIO,
+                        x2: (point.curve.x2 as number) / INCH_PX_RATIO,
+                        y2: (point.curve.y2 as number) / INCH_PX_RATIO
                     }
                 };
             } else if (point.curve.type === "quadratic") {
                 return {
-                    x: ((point.x || 0) / INCH_PX_RATIO),
-                    y: ((point.y || 0) / INCH_PX_RATIO),
+                    x: (point.x || 0) / INCH_PX_RATIO,
+                    y: (point.y || 0) / INCH_PX_RATIO,
                     curve: {
                         type: "quadratic",
-                        x1: ((point.curve.x1 as number) / INCH_PX_RATIO),
-                        y1: ((point.curve.y1 as number) / INCH_PX_RATIO)
+                        x1: (point.curve.x1 as number) / INCH_PX_RATIO,
+                        y1: (point.curve.y1 as number) / INCH_PX_RATIO
                     }
                 };
             }
         }
         return {
-            x: ((point.x || 0) / INCH_PX_RATIO),
-            y: ((point.y || 0) / INCH_PX_RATIO)
+            x: (point.x || 0) / INCH_PX_RATIO,
+            y: (point.y || 0) / INCH_PX_RATIO
         };
     });
 };
@@ -128,6 +178,7 @@ export const addText = (slide: Pptxgen.Slide, element: IPPTTextElement) => {
     if (element.outline) {
         options.line = getOutlineOption(element.outline);
     }
+    if (element.shadow) options.shadow = getShadowOption(element.shadow);
     const textProps: Pptxgen.TextProps[] = [];
     const texts = element.content.slice(0, element.content.length - 1);
     for (const text of texts) {
@@ -149,7 +200,12 @@ export const addText = (slide: Pptxgen.Slide, element: IPPTTextElement) => {
 };
 
 export const addShape = (slide: Pptxgen.Slide, element: IPPTShapeElement) => {
-    const path = getShapePath(element.shape, element.width, element.height, true) as string;
+    const path = getShapePath(
+        element.shape,
+        element.width,
+        element.height,
+        true
+    ) as string;
     const points = formatPoints(toPoints(path));
     const options: Pptxgen.ShapeProps = {
         x: element.left / INCH_PX_RATIO,
@@ -166,7 +222,7 @@ export const addShape = (slide: Pptxgen.Slide, element: IPPTShapeElement) => {
     }
     if (element.flipH) options.flipH = element.flipH === -1;
     if (element.flipV) options.flipV = element.flipV === -1;
-    //   if (element.shadow) options.shadow = getShadowOption(element.shadow)
+    if (element.shadow) options.shadow = getShadowOption(element.shadow);
     if (element.outline?.width) options.line = getOutlineOption(element.outline);
     if (element.rotate) options.rotate = element.rotate;
     slide.addShape("custGeom" as Pptxgen.ShapeType, options);
