0a4d78e66ff4464c0bd541e69dde2050c003dd28
moneyinto <woyaoqianjin@gmail.com>
2023-05-03 14:49:18 +0800

    处理导入ppt图表数据

--- src/hooks/useImport.ts
+++ src/hooks/useImport.ts
@@ -2,10 +2,12 @@ import { OPTION_TYPE } from "@/plugins/config/options";
 import Editor from "@/plugins/editor";
 import { decrypt } from "@/utils/crypto";
 import emitter, { EmitterEvents } from "@/utils/emitter";
-import { Ref } from "vue";
+import { Ref, createVNode, render } from "vue";
 import AnalyzePPTX from "analyze-pptx";
 import { dataURLtoFile, fileMd5 } from "@/utils";
 import { ISlide } from "@/types/slide";
+import ChartRender from "@/layout/Tools/Chart/ChartRender.vue";
+import { IPPTChartElement } from "@/types/element";
 
 export default (instance: Ref<Editor> | undefined, importing: Ref<boolean>, importPercent: Ref<number>) => {
     const importMPPTX = async (file: File) => {
@@ -39,6 +41,27 @@ export default (instance: Ref<Editor> | undefined, importing: Ref<boolean>, impo
         return md5;
     };
 
+    const getChartImage = (element: IPPTChartElement): Promise<string> => {
+        return new Promise(resolve => {
+            const container = document.createElement("div");
+            const vm = createVNode(ChartRender, {
+                type: element.chartType,
+                axisTransformation: element.axisTransformation,
+                width: element.width,
+                height: element.height,
+                labels: element.data.labels,
+                legends: element.data.legends,
+                series: element.data.series
+            }, null);
+            render(vm, container);
+            setTimeout(() => {
+                const chartBase64 = vm.component?.exposed?.getChartImage();
+                container.remove();
+                resolve(chartBase64);
+            }, 1000);
+        });
+    };
+
     const importPPTX = async (file: File) => {
         importing.value = true;
         importPercent.value = 10;
@@ -69,6 +92,12 @@ export default (instance: Ref<Editor> | undefined, importing: Ref<boolean>, impo
                         const coverMd5 = await dealFile(coverBase64);
                         element.cover = coverMd5;
                     }
+                }
+
+                if (element.type === "chart") {
+                    const base64 = await getChartImage(element);
+                    const md5 = await dealFile(base64);
+                    element.src = md5;
                 }
             }
         }
