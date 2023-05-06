2d0a6714984850c4fa7b5157b0db8fe2cda03776
moneyinto <woyaoqianjin@gmail.com>
2023-05-06 15:48:34 +0800

    打包分割压缩 文件归类 提升加载速度

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, Plugin, UserConfig } from "vite";
+import { defineConfig, Plugin, UserConfig, splitVendorChunkPlugin } from "vite";
 import vue from "@vitejs/plugin-vue";
 import eslintPlugin from "vite-plugin-eslint";
 import * as path from "path";
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => {
 
     const plugins: (Plugin | Plugin[])[] = [
         vue(),
+        splitVendorChunkPlugin(),
         eslintPlugin({
             include: ["src/**/*.ts", "src/**/*.vue", "src/*.vue"]
         })
@@ -25,9 +26,30 @@ export default defineConfig(({ mode }) => {
         server: {
             host: true,
             port
+        },
+        build: {
+            outDir: "dist",
+            chunkSizeWarningLimit: 1000,
+            rollupOptions: {
+                output: {
+                    manualChunks(id) {
+                        if (id.includes("node_modules")) {
+                            return id
+                                .toString()
+                                .split("node_modules/")[1]
+                                .split("/")[0]
+                                .toString();
+                        }
+                    },
+                    chunkFileNames: "assets/js/[name]-[hash].js",
+                    entryFileNames: "assets/js/[name]-[hash].js",
+                    assetFileNames: "assets/[ext]/[name]-[hash].[ext]"
+                }
+            },
+            minify: mode === "production" ? "terser" : false
         }
     };
-    
+
     return {
         ...defaultOptions
     };
