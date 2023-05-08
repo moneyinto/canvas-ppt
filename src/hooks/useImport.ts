24684607fa86fe4bf62aa2c6493d176e42b5b84c
moneyinto <woyaoqianjin@gmail.com>
2023-05-08 15:31:41 +0800

    导入ppt失败提示

--- src/hooks/useImport.ts
+++ src/hooks/useImport.ts
@@ -8,6 +8,7 @@ import { dataURLtoFile, fileMd5 } from "@/utils";
 import { ISlide } from "@/types/slide";
 import ChartRender from "@/layout/Tools/Chart/ChartRender.vue";
 import { IPPTChartElement } from "@/types/element";
+import { message } from "ant-design-vue";
 
 export default (instance: Ref<Editor> | undefined, importing: Ref<boolean>, importPercent: Ref<number>) => {
     const importMPPTX = async (file: File) => {
@@ -63,51 +64,56 @@ export default (instance: Ref<Editor> | undefined, importing: Ref<boolean>, impo
     };
 
     const importPPTX = async (file: File) => {
-        importing.value = true;
-        importPercent.value = 10;
-        const analyzePPTX = new AnalyzePPTX();
-        const slides = await analyzePPTX.read(file);
-        await instance?.value.history.clear();
-        importPercent.value = 50;
-        for (const slide of slides) {
-            if (slide.background && slide.background.image) {
-                const base64 = slide.background.image;
-                const md5 = await dealFile(base64);
-                slide.background.image = md5;
-            }
-
-            for (const element of slide.elements) {
-                if (element.type === "image") {
-                    const base64 = element.src;
+        try {
+            importing.value = true;
+            importPercent.value = 10;
+            const analyzePPTX = new AnalyzePPTX();
+            const slides = await analyzePPTX.read(file);
+            await instance?.value.history.clear();
+            importPercent.value = 50;
+            for (const slide of slides) {
+                if (slide.background && slide.background.image) {
+                    const base64 = slide.background.image;
                     const md5 = await dealFile(base64);
-                    element.src = md5;
+                    slide.background.image = md5;
                 }
 
-                if (element.type === "audio" || element.type === "video") {
-                    const base64 = element.src;
-                    const md5 = await dealFile(base64);
-                    element.src = md5;
-                    if (element.cover) {
-                        const coverBase64 = element.cover;
-                        const coverMd5 = await dealFile(coverBase64);
-                        element.cover = coverMd5;
+                for (const element of slide.elements) {
+                    if (element.type === "image") {
+                        const base64 = element.src;
+                        const md5 = await dealFile(base64);
+                        element.src = md5;
+                    }
+
+                    if (element.type === "audio" || element.type === "video") {
+                        const base64 = element.src;
+                        const md5 = await dealFile(base64);
+                        element.src = md5;
+                        if (element.cover) {
+                            const coverBase64 = element.cover;
+                            const coverMd5 = await dealFile(coverBase64);
+                            element.cover = coverMd5;
+                        }
+                    }
+
+                    if (element.type === "chart") {
+                        const base64 = await getChartImage(element);
+                        const md5 = await dealFile(base64);
+                        element.src = md5;
                     }
                 }
-
-                if (element.type === "chart") {
-                    const base64 = await getChartImage(element);
-                    const md5 = await dealFile(base64);
-                    element.src = md5;
-                }
             }
-        }
 
-        importPercent.value = 80;
-        instance?.value.stageConfig.setSlideId(slides.length > 0 ? slides[0].id : "");
-        instance?.value.stageConfig.setSlides(slides);
-        await instance?.value.history.add(OPTION_TYPE.INIT_DB_SLIDE);
-        emitter.emit(EmitterEvents.INIT_SLIDE);
-        importing.value = false;
+            importPercent.value = 80;
+            instance?.value.stageConfig.setSlideId(slides.length > 0 ? slides[0].id : "");
+            instance?.value.stageConfig.setSlides(slides);
+            await instance?.value.history.add(OPTION_TYPE.INIT_DB_SLIDE);
+            emitter.emit(EmitterEvents.INIT_SLIDE);
+            importing.value = false;
+        } catch (error) {
+            message.error("导入失败");
+            importing.value = false;
+        }
     };
 
     return {
